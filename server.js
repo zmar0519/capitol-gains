6fd8b3af5094e7cd86c42964a32202089b9134d6
HammerNTime <hammer.dylan@gmail.com>
2021-08-05 17:13:19 -0400

    clean up skeleton

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-import 'dotenv/config.js'
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import logger from 'morgan'
-import cors from 'cors'
+import "dotenv/config.js"
+import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+import logger from "morgan"
+import cors from "cors"
 
-import { router as usersRouter } from './routes/users.js'
-import { router as authRouter } from './routes/auth.js'
+import { router as usersRouter } from "./routes/users.js"
+import { router as authRouter } from "./routes/auth.js"
 
-import './config/database.js'
+import "./config/database.js"
 
 const app = express()
 
-
 app.use(cors())
-app.use(logger('dev'))
+app.use(logger("dev"))
 app.use(express.json())
 
-app.use('/api/auth', authRouter)
-app.use('/api/users', usersRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/users", usersRouter)
 
-
-app.get('/*', function (req, res) {
-  res.sendFile(
-    path.dirname(fileURLToPath(import.meta.url), 'build', 'index.html')
-  )
+app.get("/*", function (req, res) {
+	res.sendFile(
+		path.dirname(fileURLToPath(import.meta.url), "build", "index.html")
+	)
 })
 
 const port = process.env.PORT || 3001
 
 app.listen(port, () => {
-  console.log(`Express is listening on port ${port}.`)
+	console.log(`Express is listening on port ${port}.`)
 })
