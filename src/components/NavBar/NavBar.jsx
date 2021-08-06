6f44babaa75159713320738a3434eb2e599e1095
HammerNTime <hammer.dylan@gmail.com>
2021-08-06 18:06:35 -0400

    screw with nav bar buttons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,25 +8,28 @@ const NavBar = ({ user, handleLogout }) => {
 			<div className="nav-ul-container">
 			{user ? (
 				<div className="nav-item-container">
-					<div className="welcome">Welcome, {user.handle}</div>
+					<div className="title">Capitol Gains</div>
 					<div className="right-nav-btn-container">
+					<div className="welcome">Welcome, {user.handle}</div>
 						<div>
-							<NavLink to="/myProfile" className="my-profile">My Profile</NavLink>
+							<NavLink to="/myProfile" className="my-profile"><button className="my-profile-btn">My Profile</button></NavLink>
 						</div>
 						<div>
-							<NavLink to='' onClick={handleLogout} className="log-out">Log out</NavLink>
+							<NavLink to='' onClick={handleLogout} className="log-out"><button className="log-out-btn">Log out</button></NavLink>
 						</div>
 					</div>
 				</div>
 			) : (
 				<div className="nav-item-container">
-					<div className="welcome">Welcome!</div>
+					<div className="title">Capitol Gains</div>
+
 					<div className="right-nav-btn-container">
+						<div className="welcome">Welcome!</div>
 						<div>
-							<NavLink to="/login" className="log-in">Log In</NavLink>
+							<NavLink to="/login" className="log-in"><button>Log In</button></NavLink>
 						</div>
 						<div>
-							<NavLink to="/signup" className="sign-up">Sign Up</NavLink>
+							<NavLink to="/signup" className="sign-up"><button>Sign Up</button></NavLink>
 						</div>
 					</div>
 				</div>
