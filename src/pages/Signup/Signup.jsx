2df9ec3cd0a7dda0dab683aae3d15f997feb8f66
xxnsummer@gmail.com <xxnsummer@gmail.com>
2021-08-06 07:49:48 -0400

    experiment with signup jsx

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react'
-import SignupForm from '../../components/SignupForm/SignupForm'
-import styles from './Signup.module.css'
+import React from "react";
+import "./Auth.css";
 
-const Signup = (props) => {
-  const [message, setMessage] = useState()
+// Assets
+import moon from "../../assets/avatars/moon.png";
 
-  const updateMessage = msg => {
-    setMessage(msg)
-  }
+// Services
+// we will import a sign up service momentarily
 
+const SignUp = (props) => {
   return (
-    <main className={styles.container}>
-      <h1>Join the Club</h1>
-      {message && <p>{message}</p> }
-      <SignupForm 
-        updateMessage={updateMessage}
-        handleSignupOrLogin={props.handleSignupOrLogin}
-      />
-    </main>
-  )
-}
+    <div className="signup-page">
+      <div className="left-container">
+        <div className="form-container">
+          <div className="title-container">
+            <h1>Create an Account</h1>
+          </div>
+          <form className="register-form">Inputs Here</form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default SignUp;
+
+// import React, { useState } from 'react'
+// import SignupForm from '../../components/SignupForm/SignupForm'
+// import styles from './Signup.module.css'
+
+// const Signup = (props) => {
+//   const [message, setMessage] = useState()
+
+//   const updateMessage = msg => {
+//     setMessage(msg)
+//   }
+
+//   return (
+//     <main className={styles.container}>
+//       <h1>Join the Club</h1>
+//       {message && <p>{message}</p> }
+//       <SignupForm
+//         updateMessage={updateMessage}
+//         handleSignupOrLogin={props.handleSignupOrLogin}
+//       />
+//     </main>
+//   )
+// }
+
+// export default Signup
