026ad1c33cd3c12fffd07da488a7cbfb294f17e5
HammerNTime <hammer.dylan@gmail.com>
2021-08-05 22:45:37 -0400

    prep api's for auth token use

--- src/services/houseApiService.js
+++ src/services/houseApiService.js
@@ -1,10 +1,15 @@
-import * as tokenService from '../services/tokenService';
+// import * as tokenService from '../services/tokenService';
 
 const BASE_URL =
 	"https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
 
 function getAllHouseApi() {
-	return fetch(BASE_URL).then((res) => res.json())
+	return fetch(BASE_URL, {
+        // headers: {
+    //   'Authorization': `Bearer ${tokenService.getToken()}`
+    // },
+    }
+  ).then((res) => res.json())
 }
 
 export { getAllHouseApi }
