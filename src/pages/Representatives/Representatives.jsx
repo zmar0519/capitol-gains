719f45f30453bd202f08390237b510e997ea2c0b
xxnsummer@gmail.com <xxnsummer@gmail.com>
2021-08-10 00:04:52 -0400

    complete repheader

--- src/pages/Representatives/Representatives.jsx
+++ src/pages/Representatives/Representatives.jsx
@@ -2,27 +2,39 @@ import React, { useState, useEffect } from 'react'
 import { Link, useHistory, withRouter } from "react-router-dom"
 import SideMenu from '../../components/SideMenu/SideMenu'
 import "./Representatives.css"
+import RepHeader from "../../components/RepHeader/RepHeader"
 
 
 const Representatives = (props) => {
-
+  
   return (
     <div className="main-container">
       <div className="menu">
         <SideMenu />
       </div>
-      <div className="all-representatives-container">
-        {props.representativeList.map(representative => (
-          <Link to={`/representatives/` + representative.name}>
-            <div className="representative-container">
-              <div className="head-shot"><img className="head-shot" src={representative.image} alt={`${representative.name} head-shot`} /></div>
-              <div className="representative-name">{representative.name}</div>
-            </div>
-          </Link>
-        ))}
+      <div className="right-side">
+        <div className="rep-header">
+          <RepHeader/>
+        </div>
+        <div className="all-representatives-container">
+          {props.representativeList.map((representative) => (
+            <Link to={`/representatives/` + representative.name}>
+              <div className="representative-container">
+                <div className="head-shot">
+                  <img
+                    className="head-shot"
+                    src={representative.image}
+                    alt={`${representative.name} head-shot`}
+                  />
+                </div>
+                <div className="representative-name">{representative.name}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 
