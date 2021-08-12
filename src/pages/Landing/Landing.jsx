10ada0f44374bff53036d84c8de41e0905c4b244
HammerNTime <hammer.dylan@gmail.com>
2021-08-12 16:13:43 -0400

    ladding fix

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,10 @@ import landingImg from "../../assets/LandingPage/window.png"
 
 const Landing = ({ user }) => {
 	return (
-		<img id="landing-img" src={landingImg} alt="" />
+		<div>
+			<img id="landing-img" src={landingImg} alt="" />
+
+		</div>
 	)
 }
 
